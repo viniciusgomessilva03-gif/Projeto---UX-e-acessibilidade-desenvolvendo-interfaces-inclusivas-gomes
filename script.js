401b306be81dd05baf5aaaf652eed2d0c667e11b
viniciusgomessilva03-gif <vinicius.gomes.silva03@escola.pr.gov.br>
2026-08-04 09:41:27 -0300

    criação site

--- script.js
+++ script.js
@@ -1,79 +1,18 @@
-// CÓDIGO DO MODAL
-let btnAjuda = document.querySelector(".botao-ajuda");
-let btnFechar = document.querySelector(".botao-fechar");
-let modal = document.querySelector(".modal-fundo");
+// LÓGICA EM JAVASCRIPT PARA AJUSTE DE FONTE
+const TAMANHO_PADRAO = 22;
+let tamanhoAtual = TAMANHO_PADRAO;
 
-btnAjuda.addEventListener("click", abreModal);
-btnFechar.addEventListener("click", fechaModal);
+function alterarTamanhoTexto(delta) {
+  tamanhoAtual += delta;
+  
+  // Limites de tamanho (Mínimo 18px e Máximo 38px)
+  if (tamanhoAtual < 18) tamanhoAtual = 18;
+  if (tamanhoAtual > 38) tamanhoAtual = 38;
 
-function abreModal() {
-    modal.style.display = "block";
+  document.body.style.fontSize = tamanhoAtual + 'px';
 }
 
-function fechaModal() {
-    modal.style.display = "none";
-}
-
-
-// TAMANHO DE FONTES
-let tamanhoFonteAtual = 16;
-const valorAdicionado = 2;
-const valorSubtraido = 2;
-
-let btnAumentaFonte = document.getElementById("btnAumentaTexto");
-let btnDiminuiFonte = document.getElementById("btnDiminuiTexto");
-
-btnAumentaFonte.addEventListener("click", aumentaFonte);
-btnDiminuiFonte.addEventListener("click", diminuiFonte);
-
-function aumentaFonte() {
-    tamanhoFonteAtual = tamanhoFonteAtual + valorAdicionado;
-    document.documentElement.style.fontSize = `${tamanhoFonteAtual}px`;
-}
-
-function diminuiFonte() {
-    tamanhoFonteAtual = tamanhoFonteAtual - valorSubtraido;
-    document.documentElement.style.fontSize = `${tamanhoFonteAtual}px`;
-}
-
-// LEITURA DE TELA
-
-let lendo = false;
-
-let btnLeitura = document.querySelector(".botao-leitura");
-
-btnLeitura.addEventListener("click", lerEmVozAlta);
-
-function lerEmVozAlta() {
-
-    // se já está lendo
-    if (lendo == true) {
-
-        // se estiver pausado 
-        if (speechSynthesis.paused == true){
-            // continua de onde parou
-            speechSynthesis.resume();
-        } else {
-            // pausa
-            speechSynthesis.pause();
-        }
-        return;
-    }
-
-    let conteudo = document.querySelector("main");
-    let texto = conteudo.innerText;
-
-    let fala = new SpeechSynthesisUtterance(texto);
-
-    fala.lang = "pt-BR";
-    fala.onend = finalizarLeitura;
-
-    lendo = true
-
-    speechSynthesis.cancel();
-    speechSynthesis.speak(fala);
-}
-
-function finalizarLeitura() {
-    lendo = false;
+function resetarTamanhoTexto() {
+  tamanhoAtual = TAMANHO_PADRAO;
+  document.body.style.fontSize = TAMANHO_PADRAO + 'px';
 }
